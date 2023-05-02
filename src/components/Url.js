d9136f6838151b537425a0ee9864e2d93c9b8cf2
thanseerthansi <thanseermedammal720@gmail.com>
2023-05-02 11:22:08 +0530

    again update

--- src/components/Url.js
+++ src/components/Url.js
@@ -2,5 +2,5 @@
 // export const imageURL = 'http://127.0.0.1:8000'
 // export const BaseURL = 'https://jetronics.foxiomlabs.com/'
 // export const imageURL = 'https://jetronics.foxiomlabs.com'
-export const BaseURL = 'http://server.bioaqua.ae'
+export const BaseURL = 'http://server.bioaqua.ae/'
 export const imageURL = 'http://server.bioaqua.ae'
